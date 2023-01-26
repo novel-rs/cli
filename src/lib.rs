2a5b3b081e462485af1b684e224d9122c44a271e
Terakomari <im.terakomari@icloud.com>
2023-01-27 07:49:32 +0800

    refactor: remove unnecessary String construction

--- src/lib.rs
+++ src/lib.rs
@@ -21,7 +21,7 @@ pub static LANG_ID: Lazy<LanguageIdentifier> = Lazy::new(|| {
         Ok(lang_id) => lang_id,
         Err(error) => {
             eprintln!("Failed to parse LanguageIdentifier: {error}, use `en-US`");
-            String::from("en-US").parse::<LanguageIdentifier>().unwrap()
+            "en-US".parse::<LanguageIdentifier>().unwrap()
         }
     }
 });
